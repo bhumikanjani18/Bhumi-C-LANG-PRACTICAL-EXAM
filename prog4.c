d0b76f153531e27ad535ab0b850664fd87b9cbe8
bhumikanjani18 <kanjanibhumi27@gmail.com>
2025-11-14 11:31:11 +0530

    Add files via upload

--- prog4.c
+++ prog4.c
@@ -1,28 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
+void swap(int *p1, int *p2)
 {
-    int *p1, *p2;
-
-    int a,b;
-    printf("Enter value a: ");
-    scanf("%d",&a);
-
-    printf("Enter value b: ");
-    scanf("%d",&b);
-
-    p1=&a;
-    p2=&b;
-
-    printf("\nBefore swapping value of 'a': %d",*p1);
-    printf("\nBefore swapping value of 'b': %d\n",*p2);
-
     *p1 = *p1 + *p2;
     *p2 = *p1 - *p2;
     *p1 = *p1 - *p2;
+}
 
-    printf("\nAfter swapping value of 'a': %d",*p1);
-    printf("\nAfter swapping value of 'b': %d",*p2);
+int main()
+{
+    int a, b;
+
+    printf("Enter value a: ");
+    scanf("%d", &a);
+
+    printf("Enter value b: ");
+    scanf("%d", &b);
+
+    printf("\nBefore swapping value of a: %d", a);
+    printf("\nBefore swapping value of b: %d\n", b);
+
+    swap(&a, &b);
+
+    printf("\nAfter swapping value of a: %d", a);
+    printf("\nAfter swapping value of b: %d\n", b);
 
     return 0;
 }
+
